c2b749ed9c57e33ca012335500a652418f52af25
trentsteenholdt-purple <91103207+trentsteenholdt-purple@users.noreply.github.com>
2021-09-21 11:47:42 +0000

    Deploying to gh-pages from  @ 2db17343fcf18c15852e7c362ab72a2838d62aaa 🚀

---
+++ _app/pages/index.svelte-5380a0ee.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as t,L as a,d as r,F as l}from"../chunks/vendor-2e27399c.js";function n(s){return{c(){this.h()},l(s){a('[data-svelte="svelte-xlvkwj"]',document.head).forEach(r),this.h()},h(){document.title="Telstra Purple Interviews"},m:l,p:l,i:l,o:l,d:l}}class o extends s{constructor(s){super(),e(this,s,null,n,t,{})}}export{o as default};
