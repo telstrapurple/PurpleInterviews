6f6867a627f623d802b7e1dbc0a82bc7b25f69a0
Trent Steenholdt <trent.steenholdt@purple.telstra.com>
2021-09-22 15:44:27 +0800

    Prettier

--- src/routes/index.json.ts
+++ src/routes/index.json.ts
@@ -14,19 +14,17 @@ export const get: RequestHandler<InterviewDefinition[]> = async () => {
 	const InterviewFiles = await fs.readdir(InterviewFileDir);
 
 	const Interviews = await Promise.all(
-		InterviewFiles
-			.filter((fileName) => /.+\.md$/.test(fileName))
-			.map(async (fileName) => {
-				const fileContents = await fs.readFile(join(InterviewFileDir, fileName), {
-					encoding: 'utf8'
-				});
+		InterviewFiles.filter((fileName) => /.+\.md$/.test(fileName)).map(async (fileName) => {
+			const fileContents = await fs.readFile(join(InterviewFileDir, fileName), {
+				encoding: 'utf8'
+			});
 
-				const { metadata } = await process(fileContents);
-				return {
-					metadata,
-					slug: fileName.replace(/\.md$/, '')
-				};
-			})
+			const { metadata } = await process(fileContents);
+			return {
+				metadata,
+				slug: fileName.replace(/\.md$/, '')
+			};
+		})
 	);
 
 	Interviews.sort((a, b) => a.metadata.sortorder - b.metadata.sortorder);
